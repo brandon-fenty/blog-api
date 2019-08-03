4a6b9f26abd712a836833e729c7dbea2a11d6c92
brandon-fenty <brandonfenty@gmail.com>
2019-08-03 09:17:37 -0700

    controller

--- src/main/java/com/alpinlife/blog/controller/Controller.java
+++ src/main/java/com/alpinlife/blog/controller/Controller.java
@@ -1,4 +1,15 @@
 package com.alpinlife.blog.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class Controller {
+
+    @GetMapping
+    public String getAll() {
+        return "Hello World";
+    }
 }
