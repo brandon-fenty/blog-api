f0a74786ec844e0f8dd84df0e894c0b794b47eb1
brandon-fenty <brandonfenty@gmail.com>
2019-08-03 09:28:51 -0700

    returning data from mongo

--- src/main/java/com/alpinlife/blog/controller/Controller.java
+++ src/main/java/com/alpinlife/blog/controller/Controller.java
@@ -1,15 +1,27 @@
 package com.alpinlife.blog.controller;
 
+import com.alpinlife.blog.dao.BlogDao;
+import com.alpinlife.blog.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
+    @Autowired
+    private BlogDao blogDao;
+
+    public Controller(BlogDao blogDao) {
+        this.blogDao = blogDao;
+    }
+
     @GetMapping
-    public String getAll() {
-        return "Hello World";
+    public List<Post> getAll() {
+        return blogDao.findAll();
     }
 }
--- src/main/java/com/alpinlife/blog/dao/BlogDao.java
+++ src/main/java/com/alpinlife/blog/dao/BlogDao.java
@@ -1,4 +1,9 @@
 package com.alpinlife.blog.dao;
 
-public interface BlogDao {
+import com.alpinlife.blog.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogDao extends MongoRepository<Post, String> {
 }
